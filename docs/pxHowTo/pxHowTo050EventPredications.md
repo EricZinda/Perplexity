bad0b4ada29a66a56c4858afbaa354b91c1cff63
Eric Zinda <ericz@inductorsoftware.com>
2024-10-08 15:22:46 -0700

    update pxHowTo050EventPredications

--- docs/pxHowTo/pxHowTo050EventPredications.md
+++ docs/pxHowTo/pxHowTo050EventPredications.md
@@ -3,7 +3,7 @@ So far, we have been conveniently ignoring event variables (variables that start
 
 For example, we've been talking about a `_large_a_1(e2,x3), _file_n_of(x3)` and have ignored what the event variable `e2` is for. We can't do this if we are trying to find a `_very_x_deg(e2, e1), _large_a_1(e1,x3), _file_n_of(x3)`.
 
-In an MRS, any predication except a quantifier is said to "introduce" its first argument. If you look closely, you'll see that every variable is only "introduced" by one predication in a given MRS -- only one predication has a given variable as its first argument.  So, in a sense, that variable *represents* that predication.  If a predication like `_large_a_1(e2,x3)` introduces event variable `e2`, it does so to provide a place for other predications to hang modifiers.
+In an MRS, any predication except a quantifier is said to "introduce" its first argument. If you look closely, you'll see that every variable is only "introduced" by one predication in a given MRS -- only one predication has a given variable as its first argument.  So, in a sense, that variable *represents* that predication.  If a predication like `_large_a_1(e2,x3)` introduces event variable `e2`, it does so to provide a place for other predications to hang modifiers to it.
 
 For example, to represent something that is "very large", the word "very" needs to be able to attach its "veryness" to "large". For "very very large", a chain is needed where the first "very" modifies the second, which modifies "large". This is all done with events, and it happens like this because human languages allow all kinds of recursive constructions. DELPH-IN needed a way to model the behavior.
 
@@ -31,20 +31,23 @@ The `add_to_e()` method looks like this:
 def add_to_e(self, event_name, key, value):
     ...
 ~~~
+
 `add_to_e()` adds the key/value pair it is given to a dictionary that represents the event state and returns a new `State` object, just like `set_x()` does. But, unlike `set_x()`, it *adds* to whatever was in the event variable before instead of replacing it. This allows information to get built up in the event.
 
-Now, using the new `add_to_e()` method, let's create a `_very_x_deg` predication along with a `_large_a_1` predication that pays attention to modifications to its event.
+Using the `add_to_e()` method, let's create a `_very_x_deg` predication along with a `_large_a_1` predication that pays attention to modifications to its event.
 
 Let's start with `very_x_deg`:
+
 ~~~
 @Predication(vocabulary, names=["_very_x_deg"])
-def very_x_deg(state, e_introduced_binding, e_target_binding):
+def very_x_deg(context, state, e_introduced_binding, e_target_binding):
     # We'll interpret every "very" as meaning "one order of magnitude larger"
-    yield state.add_to_e(e_target_binding.variable.name, 
-                         "DegreeMultiplier", 
-                         {"Value": 10, 
-                          "Originator": execution_context().current_predication_index()})
+    yield state.add_to_e(e_target_binding.variable.name,
+                         "DegreeMultiplier",
+                         {"Value": 10,
+                          "Originator": context.current_predication_index()})
 ~~~
+
 `very_x_deg()` doesn't have any `x` variables, so we don't have to consider if they are bound or unbound. Event variables *always* exist. Predications like this simply have to add information to the `e` variable they are modifying. Recall that the first argument (in this case, `e_introduced_binding`) represents *this* predication, so the variable being modified is the second one: `e_target_binding`.
 
 It really doesn't matter *what* the predication adds to `e_target_binding` as long as the predications that consume it know what to look for. It is application specific. However, the system will look for the `Originator: <index>` field and use it to produce nice errors if it exists. 
@@ -56,7 +59,7 @@ We can now build a helper that knows about the names we've chosen so that any pr
 ~~~
 # This is a helper function that any predication that can
 # be "very'd" can use to understand just how "very'd" it is
-def degree_multiplier_from_event(state, e_introduced_binding):
+def degree_multiplier_from_event(context, state, e_introduced_binding):
     # if a "very" is modifying this event, use that value
     # otherwise, return 1
     if e_introduced_binding.value is None or \
@@ -72,23 +75,25 @@ def degree_multiplier_from_event(state, e_introduced_binding):
 The helper function gets passed an event variable and looks for the information that `very_x_deg` adds to it. If found, it returns it. Otherwise, it just returns 1. Now, any predication can multiply numbers by this value.  Here's an example of a modified `large_a_1` that now uses it:
 
 ~~~
-def large_a_1(state, e_introduced_binding, x_target_binding):
+
+@Predication(vocabulary, names=["_large_a_1"])
+def large_a_1_very(context, state, e_introduced_binding, x_target_binding):
     # See if any modifiers have changed *how* large we should be
-    degree_multiplier = degree_multiplier_from_event(state, e_introduced_binding)
+    degree_multiplier = degree_multiplier_from_event(context, state, e_introduced_binding)
 
     def criteria_bound(value):
         if degree_multiplier == 1 and value == "file2.txt":
             return True
 
         else:
-            report_error(["adjectiveDoesntApply", "large", x_target_binding.variable.name])
+            context.report_error(["adjectiveDoesntApply", "large", x_target_binding.variable.name])
             return False
 
     def unbound_values():
         if criteria_bound("file2.txt"):
             yield "file2.txt"
 
-    yield from combinatorial_style_predication_1(state, x_target_binding, criteria_bound, unbound_values)
+    yield from combinatorial_predication_1(context, state, x_target_binding, criteria_bound, unbound_values)
 ~~~
 We have modified the `large_a_1` implementation from the first section to now pay attention to "very". For the example, we assume that "file2.txt" is just large, not very large.
 
@@ -114,7 +119,7 @@ def large_a_1(state, e_introduced_binding, x_target_binding):
     ...
 ~~~
 
-The `handles=[]` clause lists out all of the keys that the predication knows how to process in its introduced event. In this case, we have also added `EventOption.optional` to say that large doesn't *require* "very", but understands it if it exists. Now it will properly process the phrase.
+The `handles=[]` clause lists out all the keys that the predication knows how to process in its introduced event. In this case, we have also added `EventOption.optional` to say that large doesn't *require* "very", but understands it if it exists. Now it will properly process the phrase.
 
 # Example
 Adding these to `hello_world.py` allows us to have this interaction:
--- hello_world/hello_world_pre_FileSystemState.py
+++ hello_world/hello_world_pre_FileSystemState.py
@@ -1,25 +1,66 @@
-from perplexity.predications import combinatorial_predication_1, lift_style_predication_2, \
-    individual_style_predication_1
-from perplexity.sstring import s
+from perplexity.predications import combinatorial_predication_1, lift_style_predication_2
 from perplexity.state import State
 from perplexity.system_vocabulary import system_vocabulary
 from perplexity.user_interface import UserInterface
-from perplexity.vocabulary import Vocabulary, Predication, ValueSize, EventOption
-import perplexity.messages
+from perplexity.vocabulary import Predication, ValueSize, EventOption
 from perplexity.world_registry import register_world
+import perplexity.messages
+import perplexity.sstring
 
 vocabulary = system_vocabulary()
+
+
+@Predication(vocabulary, names=["_file_n_of"])
+def file_n_of(context, state, x_binding, i_binding):
+    def bound_variable(value):
+        if value in ["file1.txt", "file2.txt", "file3.txt"]:
+            return True
+        else:
+            context.report_error(["notAThing", x_binding.value, x_binding.variable.name])
+            return False
+
+    def unbound_variable():
+        yield "file1.txt"
+        yield "file2.txt"
+        yield "file3.txt"
+
+    yield from combinatorial_predication_1(context,
+                                           state,
+                                           x_binding,
+                                           bound_variable,
+                                           unbound_variable)
+
+
+@Predication(vocabulary, names=["_large_a_1"])
+def large_a_1(context, state, e_introduced_binding, x_target_binding):
+    def criteria_bound(value):
+        if value == "file2.txt":
+            return True
+
+        else:
+            context.report_error(["adjectiveDoesntApply", "large", x_target_binding.variable.name])
+            return False
+
+    def unbound_values():
+        # Find all large things
+        yield "file2.txt"
+
+    yield from combinatorial_predication_1(context,
+                                           state,
+                                           x_target_binding,
+                                           criteria_bound,
+                                           unbound_values)
 
 
 @Predication(vocabulary,
              names=["_lift_v_cause"],
              arguments=[("e",), ("x", ValueSize.all), ("x", ValueSize.all)])
-def lift(state, e_introduced_binding, x_actor_binding, x_item_binding):
+def lift(context, state, e_introduced_binding, x_actor_binding, x_item_binding):
     def check_items_lifting_items(item1, item2):
         if item1 == ("Elsa", "Seo-Yun") and len(item2) == 1 and item2[0] == "table1":
             return True
         else:
-            report_error(["xIsNotYZ", x_actor_binding.variable.name, "lifting", x_item_binding.variable.name])
+            context.report_error(["xIsNotYZ", x_actor_binding.variable.name, "lifting", x_item_binding.variable.name])
 
     def all_item1s_lifting_item2s(item2):
         if len(item2) == 1 and item2[0] == "table1":
@@ -29,82 +70,49 @@ def lift(state, e_introduced_binding, x_actor_binding, x_item_binding):
         if item1 == ("Elsa", "Seo-Yun"):
             yield ("table1",)
 
-    yield from lift_style_predication_2(state, x_actor_binding, x_item_binding,
-                                        check_items_lifting_items, all_item1s_lifting_item2s,
+    yield from lift_style_predication_2(context,
+                                        state,
+                                        x_actor_binding,
+                                        x_item_binding,
+                                        check_items_lifting_items,
+                                        all_item1s_lifting_item2s,
                                         all_item2s_being_lifted_by_item1s)
 
 
 @Predication(vocabulary, names=["_student_n_of"])
-def student_n_of(state, x_binding, i_binding):
+def student_n_of(context, state, x_binding, i_binding):
     def bound_variable(value):
         if value in ["Elsa", "Seo-Yun"]:
             return True
         else:
-            report_error(["notAThing", x_binding.value, x_binding.variable.name])
+            context.report_error(["notAThing", x_binding.value, x_binding.variable.name])
             return False
 
     def unbound_variable():
         yield "Elsa"
         yield "Seo-Yun"
 
-    yield from combinatorial_predication_1(state, x_binding, bound_variable, unbound_variable)
+    yield from combinatorial_predication_1(context, state, x_binding, bound_variable, unbound_variable)
 
 
 @Predication(vocabulary, names=["_table_n_1"])
-def table_n_1(state, x_binding):
+def table_n_1(context, state, x_binding):
     def bound_variable(value):
         if value in ["table1"]:
             return True
         else:
-            report_error(["notAThing", x_binding.value, x_binding.variable.name])
+            context.report_error(["notAThing", x_binding.value, x_binding.variable.name])
             return False
 
     def unbound_variable():
         yield "table1"
 
-    yield from combinatorial_predication_1(state, x_binding, bound_variable, unbound_variable)
-
-
-@Predication(vocabulary, names=["_file_n_of"])
-def file_n_of(state, x_binding, i_binding):
-    def bound_variable(value):
-        if value in state.all_individuals():
-            return True
-        else:
-            report_error(["notAThing", x_binding.value, x_binding.variable.name])
-            return False
-
-    def unbound_variable():
-        yield from state.all_individuals()
-
-    yield from combinatorial_predication_1(state, x_binding, bound_variable, unbound_variable)
-
-
-@Predication(vocabulary,
-             names=["_large_a_1"],
-             handles=[("DegreeMultiplier", EventOption.optional)])
-def large_a_1(state, e_introduced_binding, x_target_binding):
-    # See if any modifiers have changed *how* large we should be
-    degree_multiplier = degree_multiplier_from_event(state, e_introduced_binding)
-
-    def criteria_bound(value):
-        if degree_multiplier == 1 and value == "file2.txt" and "file2.txt" in state.all_individuals():
-            return True
-
-        else:
-            report_error(["adjectiveDoesntApply", "large", x_target_binding.variable.name])
-            return False
-
-    def unbound_values():
-        if criteria_bound("file2.txt"):
-            yield "file2.txt"
-
-    yield from combinatorial_predication_1(state, x_target_binding, criteria_bound, unbound_values)
+    yield from combinatorial_predication_1(context, state, x_binding, bound_variable, unbound_variable)
 
 
 # This is a helper function that any predication that can
 # be "very'd" can use to understand just how "very'd" it is
-def degree_multiplier_from_event(state, e_introduced_binding):
+def degree_multiplier_from_event(context, state, e_introduced_binding):
     # if a "very" is modifying this event, use that value
     # otherwise, return 1
     if e_introduced_binding.value is None or \
@@ -118,61 +126,50 @@ def degree_multiplier_from_event(state, e_introduced_binding):
 
 
 @Predication(vocabulary, names=["_very_x_deg"])
-def very_x_deg(state, e_introduced_binding, e_target_binding):
+def very_x_deg(context, state, e_introduced_binding, e_target_binding):
     # We'll interpret every "very" as meaning "one order of magnitude larger"
     yield state.add_to_e(e_target_binding.variable.name,
                          "DegreeMultiplier",
                          {"Value": 10,
-                          "Originator": execution_context().current_predication_index()})
+                          "Originator": context.current_predication_index()})
 
 
-@Predication(vocabulary, names=["_delete_v_1"])
-def delete_v_1_comm(state, e_introduced_binding, x_actor_binding, x_what_binding):
-    def criteria(value):
-        # Only allow deleting files and folders that exist
-        # value will not be a tuple since individual_style_predication_1() was used
-        if value in state.all_individuals():
+
+@Predication(vocabulary,
+             names=["_large_a_1"],
+             handles=[("DegreeMultiplier", EventOption.optional)])
+def large_a_1_very(context, state, e_introduced_binding, x_target_binding):
+    # See if any modifiers have changed *how* large we should be
+    degree_multiplier = degree_multiplier_from_event(context, state, e_introduced_binding)
+
+    def criteria_bound(value):
+        if degree_multiplier == 1 and value == "file2.txt":
             return True
 
         else:
-            report_error(["cantDo", "delete", x_what_binding.variable.name])
+            context.report_error(["adjectiveDoesntApply", "large", x_target_binding.variable.name])
+            return False
 
-    def unbound_what():
-        report_error(["cantDo", "delete", x_what_binding.variable.name])
+    def unbound_values():
+        if criteria_bound("file2.txt"):
+            yield "file2.txt"
 
-    for success_state in individual_style_predication_1(state, x_what_binding,
-                                                        criteria, unbound_what,
-                                                        ["cantXYTogether", "delete", x_what_binding.variable.name]):
-        object_to_delete = success_state.get_binding(x_what_binding.variable.name).value[0]
-        operation = DeleteOperation(object_to_delete)
-        yield success_state.apply_operations([operation])
+    yield from combinatorial_predication_1(context, state, x_target_binding, criteria_bound, unbound_values)
 
 
-class DeleteOperation(object):
-    def __init__(self, value_to_delete):
-        self.value_to_delete = value_to_delete
-
-    def apply_to(self, state):
-        state.objects.remove(self.value_to_delete)
+# Called to initialize or reset the micro-world state
+def reset():
+    return State([])
 
 
-@Predication(vocabulary, names=["pron"])
-def pron(state, x_who_binding):
-    person = int(state.get_binding("tree").value[0]["Variables"][x_who_binding.variable.name]["PERS"])
-
-    def bound_variable(value):
-        if person == 2 and value == "computer":
-            return True
-        else:
-            report_error(["dontKnowActor", x_who_binding.variable.name])
-
-    def unbound_variable():
-        if person == 2:
-            yield "computer"
-        else:
-            report_error(["dontKnowActor", x_who_binding.variable.name])
-
-    yield from combinatorial_predication_1(state, x_who_binding, bound_variable, unbound_variable)
+# Creates the micro-world interface on startup
+# or if the user loads the world later
+def ui():
+    ui = UserInterface(world_name="SimplestExample",
+                       reset_function=reset,
+                       vocabulary=vocabulary,
+                       message_function=generate_custom_message)
+    return ui
 
 
 # Generates all the responses that predications can
@@ -184,59 +181,40 @@ def generate_custom_message(state, tree_info, error_term):
     if system_message is not None:
         return system_message
 
-    # error_term is of the form: [index, error] where "error" is another
-    # list like: ["name", arg1, arg2, ...]. The first item is the error
-    # constant (i.e. its name). What the args mean depends on the error
-    error_predicate_index = error_term[0]
-    error_arguments = error_term[1]
-    error_constant = error_arguments[0] if error_arguments is not None else "no error set"
-    arg_length = len(error_arguments) if error_arguments is not None else 0
-    arg1 = error_arguments[1] if arg_length > 1 else None
-    arg2 = error_arguments[2] if arg_length > 2 else None
-    arg3 = error_arguments[3] if arg_length > 3 else None
-    arg4 = error_arguments[4] if arg_length > 4 else None
-
-    if error_constant == "adjectiveDoesntApply":
-        return s("{A arg2} {'is':<arg2} not {*arg1}", tree_info)
-
-    elif error_constant == "notAThing":
-        # s() converts a variable name like 'x3' into the english words
-        # that it represented in the MRS
-        return s("{*arg1} is not {arg2}", tree_info)
-
-    elif error_constant == "xIsNotYZ":
-        return s("{arg1} is not {*arg2} {arg3}", tree_info)
-
-    elif error_constant == "dontKnowActor":
-        return s("I don't know who '{arg1}' is", tree_info)
-
-    elif error_constant == "cantXYTogether":
-        return s("I can't {*arg1} {arg2} *together*", tree_info)
-
     else:
-        # No custom message, just return the raw error for debugging
-        return str(error_term)
+        # error_term is of the form: [index, error] where "error" is another
+        # list like: ["name", arg1, arg2, ...]. The first item is the error
+        # constant (i.e. its name). What the args mean depends on the error
+        error_predicate_index = error_term[0]
+        error_arguments = error_term[1]
+        error_constant = error_arguments[0] if error_arguments is not None else "no error set"
+        arg_length = len(error_arguments) if error_arguments is not None else 0
+        arg1 = error_arguments[1] if arg_length > 1 else None
+        arg2 = error_arguments[2] if arg_length > 2 else None
+        arg3 = error_arguments[3] if arg_length > 3 else None
+        arg4 = error_arguments[4] if arg_length > 4 else None
 
+        if error_constant == "notAThing":
+            # s() acts like a Python "f string" but also
+            # converts a variable name like 'x3' into the english words
+            # that it represented in the MRS. The "*" in {*arg1} tells
+            # it to use the value of arg1 directly without converting it
+            # (described below)
+            return perplexity.sstring.s("{*arg1} is not {arg2}", tree_info)
 
-# Called to initialize or reset the micro-world state
-def reset():
-    return State(["file1.txt", "file2.txt", "file3.txt"])
+        elif error_constant == "adjectiveDoesntApply":
+            return perplexity.sstring.s("{arg2} is not {*arg1}", tree_info)
 
-
-# Creates the micro-world interface on startup
-# or if the user loads the world later
-def ui():
-    ui = UserInterface(world_name="SimplestExample",
-                       reset_function=reset,
-                       vocabulary=vocabulary)
-    return ui
+        else:
+            # No custom message, just return the raw error for debugging
+            return str(error_term)
 
 
 # Worlds need to be registered so the user can switch between them by name
 # and so that the engine can search for their autocorrect and other cached files
 # in the same directory where the ui() function resides
 register_world(world_name="SimplestExample",
-               module="hello_world_simplest",
+               module="hello_world_pre_FileSystemState",
                ui_function="ui")
 
 
